aa4dba25d5c73bc5fb04520364163fa656fb1d10
Phee <pheerapol.phee1080@gmail.com>
2026-04-01 04:10:50 +0700

    Update

---
+++ form/contact.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+import re 
+import requests
+
+WEBHOOK_URL = ""
+
+
+def is_valid_email(email) : #ฟังก์ชันตรวจสอบความถูกต้องของอีเมล
+    #basic regex pattern for email validation 
+    email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return re.match(email_pattern, email) is not None #เช็คจุดเริ่มต้นของข้อความ ว่า ตรงตาม pattern ไหม หากไม่จะคืนค่า none  
+
+def contact_form() : #เอาไว้ให้ views/about_me.py เรียกใช้
+    with st.form("contact_form") : 
+        name = st.text_input("First Name")
+        email = st.text_input("Email Address")
+        message = st.text_area("Your Message")
+        submit_buton = st.form_submit_button("Submit")
+        
+        if submit_buton :
+            #st.success("Message sucessfully sent!")
+            if not WEBHOOK_URL : #เช็คว่า webhook url มีค่าหรือไม่ หากมีข้อมูล == Ture , ไม่มี = False
+                st.error("Email service is not set up. Please try again later.",icon = "😓") #แสดง Error message  
+                st.stop()
+            
+            if not name :
+                st.error("please probide your name.",icon = "🧍‍♂️")
+                st.stop()            
+           
+            if not email:
+               st.error("Please provide your email address.", icon = "🤸‍♂️")
+               st.stop()
+            
+            if not is_valid_email(email) : # pattern email ถูกต้องไหม
+                st.error("Please provide a valid email address.", icon = "📧")
+                st.stop()
+                
+            if not message :
+                st.error("Please provide a message" , icon = "💬")
+                st.stop()
+                
+            # Prepare the payload and send it to the specified webhook URL
+            data = {"email": email, "name" : name, "message" : message} #สร้าง payload ที่รับค่าเข้ามา
+            response = requests.post(WEBHOOK_URL, json=data) #ให้ data ร่างเป็น json แล้วส่งไปที่ WEBHOOK_URL 
+            # response รับค่าที่ตอบกลับ ว่า 200 OK  หรือ 404 Not Found
+            
+            if response.status_code == 200 : #เช็คว่า response status code ส่งกลับมาเป็น 200 OK หรือไม่
+                st.success("Your message has been sent successfully! 🎉", icon = "✌") #success message
+            else :
+                st.error("There was an error sending your message. 😭",icon = "🤬")
--- views/about_me.py
+++ views/about_me.py
@@ -1,3 +1,44 @@
 import streamlit as st
+#st.title("About Me")
 
-st.title("About Me")
+from form.contact import contact_form #function จาก form/contact.py 
+
+@st.dialog("Contact Me") #การใาต่างโต้ตอบใช้ Decorator(@) เพื่อสร้าง Dialog เพื่อสร้างหน้าตา (Modal Dialog / Pop-up) ขึ้นมาบนแอป
+def show_contact_form() : 
+    #st.text_input("First Name") #ถูกคุมให้ขึ้นหน้าต่างโดย Decorator(@st.dialog) ที่สร้างขึ้นมา
+    contact_form() #เรียกใช้ function contact_form() ที่สร้างไว้ใน form/contact.py
+
+# --- HERO SETION ---
+
+col1 , col2 = st.columns(2,gap = "small" , vertical_alignment="top")
+
+with col1 :
+    st.image("assets/_DSC5154.JPG", width = 300)
+with col2 :
+    st.title("KaNOM_Pak", anchor=False) # achor = create link to this title
+    st.write("Welcome to my portfolio!")
+    if st.button("😎 Contact Me") :
+        show_contact_form()
+        
+    
+# --- EXPERIENCE & QUALIFICATIONS ---
+st.write("\n")
+st.subheader("Experience & Qualifications" , anchor = False)
+st.write(
+    """
+    - กินปลาดุกย่าง 20 ตัวใน 10  วินาที
+    - กินข้าวเหนียวมะม่วง 10 จานใน 5 วินาที
+    - กินส้มตำ 5 จานใน 3 วินาที
+    """
+)
+
+#--- SKILLS ---
+st.write("\n")
+st.subheader("Hard Skills" , anchor = False)
+st.write(
+    """
+    - Python
+    - Machine Learning
+    - Lua
+    """
+)
